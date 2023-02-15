8942744f4e71b8ef2ea2e9120a4d7d4ccae42542
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-15 00:32:18 +0000

    Paddle stays within camera bounds

--- Breakout/Assets/Scripts/Gameplay/Paddle.cs
+++ Breakout/Assets/Scripts/Gameplay/Paddle.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
 
 /// <summary>
 /// Controls and interaction logic for the paddle game object
@@ -8,14 +10,21 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     /// <summary>
-    /// 
+    /// The rigidybody2d componenet of the game object
     /// </summary>
     Rigidbody2D rb2d;
+
+    /// <summary>
+    /// Half the width of the game object's box collider 2d
+    /// </summary>
+    float halfBC2DWidth;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        halfBC2DWidth = GetComponent<BoxCollider2D>().size.x / 2;
     }
 
     // Update is called once per frame
@@ -28,18 +37,40 @@ public class Paddle : MonoBehaviour
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        float movement = 0;
 
         if (horizontalInput != 0)
         {
-            movement = horizontalInput * ConfigurationUtils.PaddleMoveUnitsPerSecond * Time.fixedDeltaTime;
+            float newXPosition = CalculateClampedX(
+                horizontalInput * ConfigurationUtils.PaddleMoveUnitsPerSecond * Time.fixedDeltaTime + transform.position.x
+            );
 
             rb2d.MovePosition(
                 new Vector2(
-                    transform.position.x + movement,
+                    newXPosition,
                     transform.position.y
                 )
             );
         }
+    }
+
+    // CalculateClampedX ensures the game object is kept within the screen's bounds
+    float CalculateClampedX(float newXPosition)
+    {
+        //if ((newXPosition - halfBC2DWidth * 2) <= ScreenUtils.ScreenLeft || (newXPosition + halfBC2DWidth * 2) >= ScreenUtils.ScreenRight)
+        //{
+        //    return transform.position.x;
+        //}
+
+        //return newXPosition;
+
+        if ((newXPosition - halfBC2DWidth) <= ScreenUtils.ScreenLeft)
+        {
+            newXPosition = ScreenUtils.ScreenLeft + 10 * halfBC2DWidth;
+        } else if ((newXPosition + halfBC2DWidth) >= ScreenUtils.ScreenRight)
+        {
+            newXPosition = ScreenUtils.ScreenRight - 10 * halfBC2DWidth;
+        }
+
+        return newXPosition;
     }
 }
